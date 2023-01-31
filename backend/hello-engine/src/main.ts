8fb57831396cc4f2b3a4c8042d3e0878d187e768
Math Borges <tangram.mb@gmail.com>
2023-01-31 12:57:46 -0300

    init with hello-engine
    create hello-engine
    
    Initial commit

---
+++ backend/hello-engine/src/main.ts
@@ -0,0 +1,29 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices/enums';
+import { AppModule } from './app.module';
+
+const logger = new Logger('Main');
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['kafka:9091'],
+          clientId: 'hello',
+        },
+        consumer: {
+          groupId: 'helloGroup',
+          allowAutoTopicCreation: true,
+        },
+      },
+    },
+  );
+  await app.listen();
+  logger.log('hello-engine on air');
+}
+bootstrap();
---
+++ backend/http/src/app.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, OnModuleInit } from '@nestjs/common';
+import { Client } from '@nestjs/microservices';
+import { ClientKafka } from '@nestjs/microservices/client';
+import { Transport } from '@nestjs/microservices/enums';
+import { Observable } from 'rxjs';
+
+@Controller()
+export class AppController implements OnModuleInit {
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:9091'],
+        clientId: 'hello',
+      },
+      consumer: {
+        groupId: 'helloGroup',
+        allowAutoTopicCreation: true,
+      },
+    },
+  })
+  private client: ClientKafka;
+
+  async onModuleInit() {
+    const messages = ['hello'];
+    messages.forEach(async (pattern) => {
+      this.client.subscribeToResponseOf(pattern);
+      await this.client.connect();
+    });
+  }
+  @Get()
+  getHello(): Observable<string> {
+    return this.client.send('hello', {});
+  }
+}
